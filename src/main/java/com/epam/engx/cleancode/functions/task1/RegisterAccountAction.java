905dbae5816873370df6a4e172aad102f8131d3d
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-10-08 16:50:11 +0300

    Refactor task #1

--- src/main/java/com/epam/engx/cleancode/functions/task1/RegisterAccountAction.java
+++ src/main/java/com/epam/engx/cleancode/functions/task1/RegisterAccountAction.java
@@ -10,39 +10,61 @@ import static com.epam.engx.cleancode.functions.task1.thirdpartyjar.CheckStatus.
 
 public class RegisterAccountAction {
 
-
     private PasswordChecker passwordChecker;
     private AccountManager accountManager;
 
     public void register(Account account) {
-        if (account.getName().length() <= 5){
-            throw new WrongAccountNameException();
-        }
-        String password = account.getPassword();
-        if (password.length() <= 8) {
-            throw new TooShortPasswordException();
-        }
-        if (passwordChecker.validate(password) != OK) {
-            throw new WrongPasswordException();
-        }
+        Date now = new Date();
+        validateAccount(account);
+        account.setCreatedDate(now);
+        account.setAddresses(getAllAssociatedAddresses(account));
 
-        account.setCreatedDate(new Date());
-        List<Address> addresses = new ArrayList<Address>();
-        addresses.add(account.getHomeAddress());
-        addresses.add(account.getWorkAddress());
-        addresses.add(account.getAdditionalAddress());
-        account.setAddresses(addresses);
         accountManager.createNewAccount(account);
     }
 
+    private List<Address> getAllAssociatedAddresses(Account account) {
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(account.getHomeAddress());
+        addresses.add(account.getWorkAddress());
+        addresses.add(account.getAdditionalAddress());
+        return addresses;
+    }
 
     public void setAccountManager(AccountManager accountManager) {
         this.accountManager = accountManager;
     }
 
     public void setPasswordChecker(PasswordChecker passwordChecker) {
-
         this.passwordChecker = passwordChecker;
     }
 
+    private void validateAccount(Account account) {
+        validateName(account.getName());
+        validatePassword(account.getPassword());
+    }
+
+    private void validateName(String name) {
+        int minimumRequiredNameLength = 5;
+        if (name.length() <= minimumRequiredNameLength) {
+            throw new WrongAccountNameException();
+        }
+    }
+
+    private void validatePassword(String password) {
+        validatePasswordLength(password);
+        validatePasswordStatus(password);
+    }
+
+    private void validatePasswordStatus(String password) {
+        if (passwordChecker.validate(password) != OK) {
+            throw new WrongPasswordException();
+        }
+    }
+
+    private void validatePasswordLength(String password) {
+        int minimumRequiredPasswordLength = 8;
+        if (password.length() <= minimumRequiredPasswordLength) {
+            throw new TooShortPasswordException();
+        }
+    }
 }
